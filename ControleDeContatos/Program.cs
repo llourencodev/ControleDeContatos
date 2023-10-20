b00ee8d251858e2add2dba32feacf07197363b34
Leandro Lourenço <Leandro Loureno@DESKTOP-DLE2G7C>
2023-10-20 13:12:00 -0300

    Add project files.

---
+++ ControleDeContatos/Program.cs
@@ -0,0 +1,40 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using static ControleDeContatos.Repositorio.ContatoRepositorioM;
+
+namespace ControleDeContatos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
+            builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
